8998da24bcd4b5279237bcbb1293325f554e6a69
haku1217 <haruki12.17birthday@gmail.com>
2020-08-26 02:13:30 +0900

    test sitemap change

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -15,29 +15,18 @@ const prettier = require('prettier')
     '!src/pages/api'
   ])
   console.log({ pages }, 'pages path')
-  const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pages
-              .map((page) => {
-                const path = page
-                  .replace('src/pages', '')
-                  .replace('src/posts/', '/blog/')
-                  .replace('.tsx', '')
-                  .replace('.md', '')
-                const route = path === '/index' ? '' : path
-                console.log(path, 'path')
-                return `
-                        <sitemap>
-                            <loc>${`https://haku-dev.tk${route}`}</loc>
-                            <changefreq>daily</changefreq>
-                            <priority>1.0</priority>
-                        </sitemap>
-                    `
-              })
-              .join('')}
-        </sitemapindex>
-    `
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">${pages
+    .map((page) => {
+    const path = page
+    .replace('src/pages', '')
+    .replace('src/posts/', '/blog/')
+    .replace('.tsx', '')
+    .replace('.md', '')
+    const route = path === '/index' ? '' : path
+    console.log(path, 'path')
+    return `<url><loc>${`https://haku-dev.tk${route}`}</loc><changefreq>daily</changefreq><priority>1.0</priority></url>`
+    }).join('')}</urlset>
+  `
 
   // If you're not using Prettier, you can remove this.
   const formatted = prettier.format(sitemap, {
@@ -45,12 +34,12 @@ const prettier = require('prettier')
     parser: 'html'
   })
 
-  fs.writeFileSync('public/sitemap.xml', formatted)
+  fs.writeFileSync('public/sitemap.xml', sitemap)
 })()
 
 // const sitemap = `
 // <?xml version="1.0" encoding="UTF-8"?>
-// <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+// <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 //     ${pages
 //       .map((page) => {
 //         const path = page
@@ -61,13 +50,13 @@ const prettier = require('prettier')
 //         const route = path === '/index' ? '' : path
 //         console.log(path, 'path')
 //         return `
-//                 <url>
+//                 <sitemap>
 //                     <loc>${`https://haku-dev.tk${route}`}</loc>
 //                     <changefreq>daily</changefreq>
 //                     <priority>1.0</priority>
-//                 </url>
+//                 </sitemap>
 //             `
 //       })
 //       .join('')}
-// </urlset>
+// </sitemapindex>
 // `

6ee769feb0ca2179e1f09bacb55474b4dcf212c4
haku1217 <haruki12.17birthday@gmail.com>
2020-08-26 01:55:01 +0900

    fixed sitemap gen

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -17,7 +17,7 @@ const prettier = require('prettier')
   console.log({ pages }, 'pages path')
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+        <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             ${pages
               .map((page) => {
                 const path = page
@@ -28,15 +28,15 @@ const prettier = require('prettier')
                 const route = path === '/index' ? '' : path
                 console.log(path, 'path')
                 return `
-                        <url>
+                        <sitemap>
                             <loc>${`https://haku-dev.tk${route}`}</loc>
                             <changefreq>daily</changefreq>
                             <priority>1.0</priority>
-                        </url>
+                        </sitemap>
                     `
               })
               .join('')}
-        </urlset>
+        </sitemapindex>
     `
 
   // If you're not using Prettier, you can remove this.
@@ -47,3 +47,27 @@ const prettier = require('prettier')
 
   fs.writeFileSync('public/sitemap.xml', formatted)
 })()
+
+// const sitemap = `
+// <?xml version="1.0" encoding="UTF-8"?>
+// <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+//     ${pages
+//       .map((page) => {
+//         const path = page
+//           .replace('src/pages', '')
+//           .replace('src/posts/', '/blog/')
+//           .replace('.tsx', '')
+//           .replace('.md', '')
+//         const route = path === '/index' ? '' : path
+//         console.log(path, 'path')
+//         return `
+//                 <url>
+//                     <loc>${`https://haku-dev.tk${route}`}</loc>
+//                     <changefreq>daily</changefreq>
+//                     <priority>1.0</priority>
+//                 </url>
+//             `
+//       })
+//       .join('')}
+// </urlset>
+// `
